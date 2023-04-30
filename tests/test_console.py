6fa9f70e426cc9c49af2782c47a6a8bf70d45b81
Farad078 <faradokorie@gmail.com>
2023-04-30 21:40:34 +0100

    test

--- tests/test_console.py
+++ tests/test_console.py
@@ -41,5 +41,28 @@ class TestConsoleDocs(unittest.TestCase):
                         "HBNBCommand class needs a docstring")
 
 
+class TestHBNBCommandDocs(unittest.TestCase):
+    """Class for testing BaseModel docs"""
+
+    @classmethod
+    def setUpClass(cls):
+        print('\n\n.................................')
+        print('..... Testing Documentation .....')
+        print('.......  For the Console  .......')
+        print('.................................\n\n')
+
+    def test_doc_file(self):
+        """... documentation for the file"""
+        expected = '\nCommand interpreter for Holberton AirBnB project\n'
+        actual = console.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_class(self):
+        """... documentation for the class"""
+        expected = '\nCommand inerpreter class\n'
+        actual = HBNBCommand.__doc__
+        self.assertEqual(expected, actual)
+
+
 if __name__ == "__main__":
     unittest.main()
