f4118a9f3df33bfcb1e4ab6667d7ba67c1675f9c
Farad078 <faradokorie@gmail.com>
2023-05-01 20:31:12 +0100

    4. update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,28 @@
+#!/usr/bin/python3
+"""Flask server (variable app)
+"""
+
+
 from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views
 from os import getenv
-
+from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def downtear(self):
+    '''Status of your API'''
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
 
 
 if __name__ == "__main__":
