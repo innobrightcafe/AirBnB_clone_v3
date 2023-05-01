d644d80d04bbfd33b40204e53f84592565384d57
Farad078 <faradokorie@gmail.com>
2023-05-01 20:21:14 +0100

    4. update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Close storage session"""
-    storage.close()
-
-
 if __name__ == "__main__":
-    host1 = os.environ.get("HBNB_API_HOST", '0.0.0.0')
-    port1 = int(os.environ.get("HBNB_API_PORT", 5000))
-    app.run(host=host1, port=port1, threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
